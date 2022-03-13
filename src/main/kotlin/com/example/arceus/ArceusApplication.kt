5047e495b71e4485b86ea8ac9b32928f24f6e4e2
WeiChun0723 <laiwc0723@gmail.com>
2022-03-13 11:27:07 +0800

    The Begining

---
+++ src/main/kotlin/com/example/arceus/ArceusApplication.kt
@@ -0,0 +1,11 @@
+package com.example.arceus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArceusApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArceusApplication>(*args)
+}
